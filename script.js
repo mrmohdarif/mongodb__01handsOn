1e540d56d06785c1254d608dd57a6ba443089969
mohd arif <alliswell5solution@gmail.com>
2023-08-09 22:25:38 +0530

    0.0

---
+++ script.js
@@ -0,0 +1,29 @@
+fetch("https://jsonplaceholder.typicode.com/posts")
+
+
+// let arr=[1,2,3,4,5,6]
+// const [a,s,d,...rest]=arr
+// console.log(rest);
+
+// async function api(){
+//        const data=await fetch("")
+// }
+
+
+//  how to fetch API by using asynce and await key word.
+// Objects and classes in js
+// What are generators? Why do we use it?
+// What is callback queue?
+// what are higher order functions
+// Explain Local Scope, Block Scope, Functional Scope and Scope Chain in javascript
+// What is difference between null and undefined and where to use what?
+// What are pure functions?
+// What is an event loop and call stack
+// What is prototype chain
+// What is callback hell
+// What is creation phase and execution phase?
+// promise
+// What is Asyn await
+// Call,bind and apply
+// "Explain the following - 
+// Reverse, Concat, Push, Pop, Array, let var const,and many methods in react"
